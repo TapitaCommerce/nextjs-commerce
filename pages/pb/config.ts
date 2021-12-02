9a5fc09fad051572aebd8ac90f11fd02e788bdc6
lanapone <lanapone@lanapones-MBP.lan>
2021-12-02 16:08:35 +0700

    restore token

--- pages/pb/config.ts
+++ pages/pb/config.ts
@@ -1,4 +1,3 @@
 export const TAPITA_ENDPOINT = 'https://tapita.io/pb/graphql/'
-// export const TAPITA_INTEGRATION_TOKEN = '17nMVmUJAxdditfSvAqBqoC6VJKTKpD21626949895'
-export const TAPITA_INTEGRATION_TOKEN = '29yAg9xIdZynZOiIOcZ5JlMWz3MWgHxn1629093956'
+export const TAPITA_INTEGRATION_TOKEN = '17nMVmUJAxdditfSvAqBqoC6VJKTKpD21626949895'
 
